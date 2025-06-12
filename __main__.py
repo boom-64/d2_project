19cf5f720974899b3d43217a66c4764d424213e3
boom-64 <oranqe64@gmail.com>
2025-06-12 16:23:50 +0100

    Fixed last commit: deleted mf files, deleted dupes

--- __main__.py
+++ __main__.py
@@ -1,18 +1,26 @@
 from pathlib import Path
 
-import mf_utils
-from schemas import URL
+from utils import mf_utils
+from core import schemas 
 
 api_key = 'd4705221d56b4040b8c5c6b4ebd58757'
+
 url_root = 'https://www.bungie.net'
 loc_path = '/Platform/Destiny/Manifest'
+mf_finder_url = schemas.ParsedURL.from_base_and_path(
+    base_url=url_root, 
+    path=loc_path
+)
+
 lang = 'en'
 expected_remote_lang_dir='/common/destiny_content/sqlite/'
+
 zip_path = Path('manifest.zip')
 mf_dir_path = Path('manifest')
-mf_finder_url = URL.from_base_and_path(base_url=url_root, path=loc_path)
+
 mf_ext='.content'
 bak_ext='.bak'
+
 mf_utils.update_manifest(
     key=api_key,
     dl_url_root=url_root,
